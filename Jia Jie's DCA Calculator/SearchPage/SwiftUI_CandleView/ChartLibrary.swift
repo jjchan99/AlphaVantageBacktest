b8166756d34a20c2905d332d0570515a2503ff70
Jia Jie Chan <cjj.ninetynine@gmail.com>
2021-11-27 18:58:46 +0800

    Draft

--- Jia Jie's DCA Calculator/SearchPage/SwiftUI_CandleView/ChartLibrary.swift
+++ Jia Jie's DCA Calculator/SearchPage/SwiftUI_CandleView/ChartLibrary.swift
@@ -232,7 +232,18 @@ struct ControlPoint {
     let previousPoint: CGPoint
     let nextPoint: CGPoint
     
-    func getControlPoints() -> (controlPoint1: CGPoint, controlPoint2: CGPoint) {
+    func staticControlPoints() -> (staticPoint1: CGPoint, staticPoint2: CGPoint) {
+        let x1 = previousPoint.x + (centerPoint.x - previousPoint.x) * (1 - scaleFactor)
+        let y1 = previousPoint.y + (centerPoint.y - previousPoint.y) * (1 - scaleFactor)
+        let controlPoint1: CGPoint = .init(x: x1, y: y1)
+        
+        let x2 = centerPoint.x + (nextPoint.x - centerPoint.x) * (scaleFactor)
+        let y2 = centerPoint.y + (nextPoint.y - centerPoint.y) * (scaleFactor)
+        let controlPoint2: CGPoint = .init(x: x2, y: y2)
+        return (controlPoint1, controlPoint2)
+    }
+    
+    private func getControlPoints() -> (controlPoint1: CGPoint, controlPoint2: CGPoint) {
         let x1 = previousPoint.x + (centerPoint.x - previousPoint.x) * scaleFactor
         let y1 = previousPoint.y + (centerPoint.y - previousPoint.y) * scaleFactor
         let controlPoint1: CGPoint = .init(x: x1, y: y1)
@@ -243,14 +254,15 @@ struct ControlPoint {
         return (controlPoint1, controlPoint2)
     }
     
-    func translateControlPoints(_ cp: (controlPoint1: CGPoint, controlPoint2: CGPoint)) -> (controlPoint1: CGFloat, controlPoint2: CGFloat){
+    func translateControlPoints() -> (controlPoint1: CGPoint, controlPoint2: CGPoint) {
+        let cp = getControlPoints()
         let MM: CGPoint = .init(x: 2 * centerPoint.x - cp.controlPoint1.x, y: 2 * centerPoint.y - cp.controlPoint1.y)
         let NN: CGPoint = .init(x: 2 * centerPoint.x - cp.controlPoint2.x, y: 2 * centerPoint.y - cp.controlPoint2.y)
         
         let translatedControlPoint1 = CGPoint(x: (NN.x + cp.controlPoint1.x)/2, y: (NN.y + cp.controlPoint1.y)/2)
         let translatedControlPoint2 = CGPoint(x: (MM.x + cp.controlPoint2.x)/2, y: (MM.y + cp.controlPoint2.y)/2)
         
-        return ((translatedControlPoint1, translatedControlPoint2))
+        return ((controlPoint1: translatedControlPoint1, controlPoint2: translatedControlPoint2))
     }
     
 }

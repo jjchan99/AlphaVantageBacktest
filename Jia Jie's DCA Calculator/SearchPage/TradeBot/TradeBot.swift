cfdf743aa2f2c844a8c582344a7cb8b98c673c29
Jia Jie Chan <cjj.ninetynine@gmail.com>
2021-12-10 03:00:25 +0800

    draft

--- Jia Jie's DCA Calculator/SearchPage/TradeBot/TradeBot.swift
+++ Jia Jie's DCA Calculator/SearchPage/TradeBot/TradeBot.swift
@@ -38,7 +38,6 @@ struct TradeBot: CloudKitInterchangeable {
         let record = self.record
         //DO STUFF WITH THE RECORD
         
-        CloudKitUtility.update(item: <#T##CloudKitInterchangeable#>, completion: <#T##(Result<Bool, Error>) -> Void#>)
         return TradeBot(record: record)!
     }
     
@@ -149,7 +148,10 @@ final class EvaluationCondition: CloudKitInterchangeable, CustomStringConvertibl
     var record: CKRecord
     
     func update() -> EvaluationCondition {
-        return self
+        let record = self.record
+        //DO STUFF WITH THE RECORD
+        
+        return .init(record: record)!
     }
     
     let technicalIndicator: TechnicalIndicators

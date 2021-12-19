bfcc32e4318929adb01fea0682877f35648a5a1c
Jia Jie Chan <cjj.ninetynine@gmail.com>
2021-12-20 04:21:14 +0800

    Thoughts

--- Jia Jie's DCA Calculator/TradeBot/TradeBotAlgorithm.swift
+++ Jia Jie's DCA Calculator/TradeBot/TradeBotAlgorithm.swift
@@ -47,6 +47,14 @@ struct TradeBotAlgorithm {
     }
     
     //MARK: SCENARIO 1: INDICATORS BASED ON OPEN. ELEMENT IS ALWAYS MOST RECENT. SECNARIO 2: INDICATORS BASED ON CLOSE. ELEMENT IS PREVIOUS FOR INDICATORS. ELEMENT IS MOST RECENT FOR PRICE INPUT (USING OPEN).
+    
+    //MARK: UPDATED THOUGHTS: You are currently comparing most recent open to previous close, and buying on most recent close. You have options:
+    // 1 - Compare previous close to previous close. Buy on most recent open (lagging indicator, results only published at end of day based on API. Check again?)
+    // 2 - Compare most recent close to most recent close. Buy on most recent close (only 90% realistic as MOC orders need leeway)
+    // 3 - Compare previous close to previous close. Buy on most recent close (Simulate MOC order).
+    // Conclusion: Depends on aim. Realism: Option 1, 3, and current. User satisfaction (result publishing): Option 2.
+    // Current and option 3 is not much different. Current option feels inherently "More live". (Recency of input value) and hence current option is better.
+    // Alternative: Compare most recent open to most recent open, buy on most recent close. (Change indicator parameter as 'open'). Again, slight advantage is recency. (Recency of indicator value from previous to most recent).
     static func getInputValue<T: Comparable>(i: TechnicalIndicators, element: OHLCCloudElement) -> T? {
         switch i {
         case .movingAverage:

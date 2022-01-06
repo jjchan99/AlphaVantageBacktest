396f0fec47745c454c7aad4243e126b30d221f52
Jia Jie Chan <cjj.ninetynine@gmail.com>
2022-01-07 02:46:39 +0800

    Draft

--- Jia Jie's DCA Calculator/InputViews/InputViewModel.swift
+++ Jia Jie's DCA Calculator/InputViews/InputViewModel.swift
@@ -206,6 +206,7 @@ class InputViewModel: ObservableObject {
         case "MA":
             section = 0
             index = 0
+            selectedTabIndex = 0
         case "BB":
             section = 0
             index = 1
@@ -221,6 +222,10 @@ class InputViewModel: ObservableObject {
         case "profitTarget":
             section = 1
             index = 2
+        case "MAOperation":
+            section = 0
+            index = 0
+            selectedTabIndex = 1
         default:
             fatalError()
         }
@@ -230,12 +235,14 @@ class InputViewModel: ObservableObject {
         selectedPercentage = 0
         selectedPositionIdx = 0
         selectedWindowIdx = 0
+        anotherSelectedWindowIdx = 0
         stepperValue = 2
     }
     
     func resetIndexPath() {
         section = 0
         index = 0
+        selectedTabIndex = 0
     }
     //MARK: - RESTORATION OPERATIONS
     
@@ -245,7 +252,14 @@ class InputViewModel: ObservableObject {
         case 0:
             switch index {
             case 0:
-                restoreMA(for: dict)
+                switch selectedTabIndex {
+                case 0:
+                    restoreMA(for: dict)
+                case 1:
+                    restoreMACrossover(for: dict)
+                default:
+                    fatalError()
+                }
             case 1:
                 restoreBB(for: dict)
             case 2:
@@ -268,6 +282,30 @@ class InputViewModel: ObservableObject {
         default:
             fatalError()
             
+        }
+    }
+    
+    func restoreMACrossover(for dict: InputRepository.Dict) {
+        let dict = repo.get(dict: dict)
+        if let input = dict["MAOperation"] {
+            let i = input.technicalIndicator
+            switch i {
+            case .movingAverageOperation(period1: let period1, period2: let period2):
+                selectedWindowIdx = window.firstIndex(of: period1)!
+                anotherSelectedWindowIdx = window.firstIndex(of: period2)!
+            default:
+                fatalError()
+            }
+        }
+        
+        if let input2 = dict["MAOperation"] {
+            let i = input2.aboveOrBelow
+            switch i {
+            case .priceBelow:
+                selectedPositionIdx = 1
+            case .priceAbove:
+                selectedPositionIdx = 0
+            }
         }
     }
     

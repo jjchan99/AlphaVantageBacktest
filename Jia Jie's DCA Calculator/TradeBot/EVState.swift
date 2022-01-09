fc428bf5bbd43e2a867a6b7746461ef48fa661a6
Jia Jie Chan <cjj.ninetynine@gmail.com>
2022-01-10 03:32:58 +0800

    EVState Draft

--- Jia Jie's DCA Calculator/TradeBot/EVState.swift
+++ Jia Jie's DCA Calculator/TradeBot/EVState.swift
@@ -7,28 +7,55 @@
 
 import Foundation
 
-protocol EvaluationState {
-    associatedtype T: Comparable
-    func targetValue() -> T
-    func currentValue() -> T
-    func setContext(context: TradeBot)
+protocol EvaluationState: AnyObject {
+    func perform() -> Bool
+    func setContext(context: [String: Any])
 }
 
 class MA_EVState: EvaluationState {
     
     typealias T = Double
+    private(set) var context: [String: Any]!
     
-    private(set) var context: TradeBot!
-    
-    func setContext(context: TradeBot) {
+    func setContext(context: [String: Any]) {
         self.context = context
     }
     
-    func targetValue() -> Double {
+    private func targetValue() -> Double {
         
     }
     
-    func currentValue() -> Double {
+    private func getTicker() -> OHLCCloudElement {
+        for item in context where item is OHLCCloudElement {
+            let item = item as! OHLCCloudElement
+            return item
+        }
+    }
+    
+    private func currentValue(window: Int) -> Double {
+        for item in context where item is EvaluationCondition {
+            let item = item as! EvaluationCondition
+            switch item.technicalIndicator {
+            case .movingAverage(period: let period):
+                return getTicker().movingAverage[period]!
+            default:
+                fatalError()
+            }
+        }
+        fatalError()
+    }
+    
+    func perform() -> Bool {
         
     }
+    
+}
+
+class Context {
+    var objectA: EvaluationState!
+    
+    func test() {
+        objectA.perform()
+    }
+
 }

defb9b087e7abd55397b5c2f460b14cd84cb4851
Jia Jie Chan <cjj.ninetynine@gmail.com>
2022-01-15 19:10:30 +0800

    Draft

--- TBTests.swift
+++ TBTests.swift
@@ -61,11 +61,9 @@ class EVTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         XCTAssertEqual(sut.perform(), true)
-        sut
-            .transition(condition: Mock.condition())
-        XCTAssert(sut.perform(), true)
-        sut
-            .transition(condition: Mock.condition())
+        sut = sut.transition(condition: Mock.condition())
+        XCTAssertEqual(sut.perform(), true)
+        sut = sut.transition(condition: Mock.condition())
         
     }
 

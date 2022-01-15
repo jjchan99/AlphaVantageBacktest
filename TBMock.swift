a841020bbc7d25798027cc16a527c99326bc5506
Jia Jie Chan <cjj.ninetynine@gmail.com>
2022-01-15 17:08:59 +0800

    Draft

---
+++ TBMock.swift
@@ -0,0 +1,26 @@
+import Foundation
+struct Mock {
+    
+    static func account() -> Account {
+        return Account(budget: 5000, cash: 5000, accumulatedShares: 0)
+    }
+    
+    static func tb() -> TradeBot {
+        return TradeBot(account: account(), conditions: [condition()])!
+    }
+    
+    static func condition() -> EvaluationCondition {
+        return EvaluationCondition(technicalIndicator: .movingAverage(period: 200), aboveOrBelow: .priceAbove, enterOrExit: .enter, andCondition: [])!
+    }
+    
+    static func ticker() -> OHLCCloudElement {
+        return OHLCCloudElement(stamp: "", open: 0, high: 0, low: 0, close: 0, volume: 0, percentageChange: nil, RSI: [:], movingAverage: [200 : 1], standardDeviation: nil, upperBollingerBand: nil, lowerBollingerBand: nil)
+    }
+    
+    static func context() -> ContextObject {
+        let o = ContextObject(account: account(), tb: tb())
+            .updateTickers(previous: ticker(), mostRecent: ticker())
+        return o
+    }
+    
+}

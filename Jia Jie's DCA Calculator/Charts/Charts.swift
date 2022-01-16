351d7ce22cc37230bffa045ac4b625c0ebbd36be
Jia Jie Chan <cjj.ninetynine@gmail.com>
2022-01-16 17:27:30 +0800

    draft

--- Jia Jie's DCA Calculator/Charts/Charts.swift
+++ Jia Jie's DCA Calculator/Charts/Charts.swift
@@ -17,18 +17,23 @@ struct Frame {
     
         adjustedWidth = width - (2 * padding)
         horizontalJumpPerIndex = adjustedWidth / CGFloat(count - 1)
-        maxWidth = 0.06 * adjustedWidth
-        spacing = horizontalJumpPerIndex <= 5.0 ? 1 : (0.5) * horizontalJumpPerIndex > maxWidth ? maxWidth : (0.5) * horizontalJumpPerIndex
     }
     
     let height: CGFloat
     let width: CGFloat
     let padding: CGFloat
-    
-    var maxWidth: CGFloat
-    var spacing: CGFloat
     var adjustedWidth: CGFloat
     var horizontalJumpPerIndex: CGFloat
+}
+
+extension Frame {
+    func spacing() -> CGFloat {
+        horizontalJumpPerIndex <= 5.0 ? 1 : (0.5) * horizontalJumpPerIndex > maxWidth() ? maxWidth() : (0.5) * horizontalJumpPerIndex
+    }
+    
+    func maxWidth() -> CGFloat {
+        0.06 * adjustedWidth
+    }
 }
 
 struct MMR<T: CustomNumeric> {
@@ -43,6 +48,14 @@ protocol RenderState {
     func updateState(index: Int)
 }
 
+protocol OpHLC {
+    associatedtype T where T: CustomNumeric
+    var open: T { get }
+    var high: T { get }
+    var low: T { get }
+    var close: T { get }
+}
+
 struct Y {
     static private func cgf<T: CustomNumeric>(_ value: T) -> CGFloat {
         return CGFloat(fromNumeric: value)
@@ -53,6 +66,19 @@ struct Y {
         let share = cgf(deviation / mmr.range)
         let scaled = share * frame.height
         return scaled
+    }
+    
+    static func get<T: OpHLC>(point: T, mmr: MMR<T.T>, frame: Frame) -> (open: CGFloat, high: CGFloat, low: CGFloat, close: CGFloat) {
+        let open = point.open
+        let high = point.high
+        let low = point.low
+        let close = point.close
+        let yOpen = (abs(cgf(open - mmr.max)) / cgf(mmr.range)) * frame.height
+        let yHigh = (abs(cgf(high - mmr.max)) / cgf(mmr.range)) * frame.height
+        let yLow = (abs(cgf(low - mmr.max)) / cgf(mmr.range)) * frame.height
+        let yClose = (abs(cgf(close - mmr.max)) / cgf(mmr.range)) * frame.height
+//        print("yOpen: \(yOpen) yHigh: \(yHigh) yLow: \(yLow) yClose: \(yClose)")
+        return ((yOpen, yHigh, yLow, yClose))
     }
 }
 
@@ -87,16 +113,43 @@ class LineState<Object: Plottable>: RenderState {
     var path = Path()
     var area = Path()
     
-    func getXPosition() {
-        
-    }
-    
-    func getYPosition() {
-        
-    }
-    
     func updateState(index: Int) {
         let x = X.get(index: index, frame: frame)
         let y = Y.get(point: data[index][keyPath: Object.keyPath!], mmr: mmr, frame: frame)
+        let point = CGPoint(x: x, y: y)
+        path.move(to: point)
+        area.move(to: point)
+    }
+}
+
+class CandleState<Object: OpHLC & Plottable>: RenderState {
+    
+    let data: [Object]
+    let frame: Frame
+    let mmr: MMR<Object.T>
+    
+    init(data: [Object], frame: Frame, mmr: MMR<Object.T>, setKeyPath: KeyPath<Object, Object.T>) {
+        self.data = data
+        self.frame = frame
+        self.mmr = mmr
+        Object.keyPath = setKeyPath
+    }
+    
+    var stick = Path()
+    var body = Path()
+    
+    func updateState(index: Int) {
+        let green = data[index].close > data[index].open
+        let x = X.get(index: index, frame: frame)
+        let y = Y.get(point: data[index], mmr: mmr, frame: frame)
+        body.move(to: .init(x: x - (0.5 * frame.spacing()), y: green ? y.close : y.open))
+        body.addLine(to: .init(x: x + (0.5 * frame.spacing()), y: green ? y.close : y.open))
+        body.addLine(to: .init(x: x + (0.5 * frame.spacing()), y: green ? y.open : y.close))
+        body.addLine(to: .init(x: x - (0.5 * frame.spacing()), y: green ? y.open : y.close))
+        body.addLine(to: .init(x: x - (0.5 * frame.spacing()), y: green ? y.close : y.open))
+     
+        stick.move(to: .init(x: x, y: y.high))
+        stick.addLine(to: .init(x: x, y: y.low))
+      
     }
 }

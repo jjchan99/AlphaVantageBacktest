fdddd00685aa5fd6139ad99f4ebc88bd50bc370d
Jia Jie Chan <cjj.ninetynine@gmail.com>
2022-01-19 02:17:28 +0800

    Progress

--- ChartTest.swift
+++ ChartTest.swift
@@ -14,10 +14,10 @@ extension Double: Plottable {
 
 class ChartTest: XCTestCase {
     
-    var sut: [Double] = [3.5, 7.5, 10, 7.5, 3.5]
+    var sut: [Double] = [3.5, 4.5, 10, 7.5, 3.5]
     var state: RenderState!
-    let frame: Frame = .init(count: 5, height: Dimensions.height, width: Dimensions.width, padding: 0.1 * Dimensions.width)
-    let mmr: MMR = .init(max: 10, min: 3.5)
+    var frame: Frame = .init(count: 5, height: Dimensions.height, width: Dimensions.width, padding: 0.1 * Dimensions.width)
+    var mmr: MMR = .init(max: 10, min: 3.5)
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -44,6 +44,10 @@ class ChartTest: XCTestCase {
         y = Y.get(point: 3.5, mmr: mmr, frame: frame)
         XCTAssertEqual(y, Dimensions.height)
         XCTAssertEqual(reverse(y: y), 3.5)
+        
+        let y1 = Y.get(point: 4.5, mmr: mmr, frame: frame)
+        let y2 = Y.get(point: 7.5, mmr: mmr, frame: frame)
+        XCTAssert(y1 > y2)
     }
 
     func testPerformanceExample() throws {

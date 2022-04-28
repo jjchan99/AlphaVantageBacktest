6039ae9d818467b1b65d9ca5cf6c9c7170ec2d43
Jia Jie Chan <cjj.ninetynine@gmail.com>
2022-04-29 03:25:44 +0800

    Draft fetch all bots

--- Jia Jie's DCA Calculator/CloudKit/CloudViewModel.swift
+++ Jia Jie's DCA Calculator/CloudKit/CloudViewModel.swift
@@ -11,7 +11,7 @@ import Combine
 
 class CloudViewModel: ObservableObject {
     var subscribers = Set<AnyCancellable>()
-    var retrievals: TradeBot?
+    @Published var retrievals: TradeBot?
     
     let height: CGFloat = CGFloat(300).hScaled()
     let width: CGFloat = CGFloat(390).wScaled()
@@ -22,40 +22,62 @@ class CloudViewModel: ObservableObject {
 struct CloudView: View {
     @EnvironmentObject var viewModel: CloudViewModel
     var body: some View {
-        ZStack {
-            VStack(spacing: 20) {
-                Button(action: {
-                    BotAccountCoordinator.upload(tb: BotAccountCoordinator.specimen()) {
-                        Log.queue(action: "Upload success")
-                    }
-                }, label: {
-                    Text("Click me")
-                })
-                Button(action: {
-                    BotAccountCoordinator.fetchBot()
-                        .receive(on: DispatchQueue.main)
-                        .sink { _ in
-                            
-                        } receiveValue: { tb in
-                            viewModel.retrievals = tb
+//        ZStack {
+//            VStack(spacing: 20) {
+//                Button(action: {
+//                    BotAccountCoordinator.upload(tb: BotAccountCoordinator.specimen()) {
+//                        Log.queue(action: "Upload success")
+//                    }
+//                }, label: {
+//                    Text("Click me")
+//                })
+//                Button(action: {
+//                    BotAccountCoordinator.fetchBot()
+//                        .receive(on: DispatchQueue.main)
+//                        .sink { _ in
+//
+//                        } receiveValue: { tb in
+//                            viewModel.retrievals = tb
+//                        }
+//                        .store(in: &viewModel.subscribers)
+//
+//                }, label: {
+//                    Text("Get the Parent.")
+//                })
+//                Button {
+//                    if viewModel.retrievals != nil {
+//                        BotAccountCoordinator.delete(tb: viewModel.retrievals!) {
+//                        print("Delete success")
+//                    }
+//                    }
+//                } label: {
+//                    Text("Delete the tb")
+//                }
+                NavigationView {
+                Form {
+                    Section {
+                        if viewModel.retrievals != nil {
+                            Text("Something came up.")
                         }
-                        .store(in: &viewModel.subscribers)
-
-                }, label: {
-                    Text("Get the Parent.")
-                })
-                Button {
-                    if viewModel.retrievals != nil {
-                        BotAccountCoordinator.delete(tb: viewModel.retrievals!) {
-                        print("Delete success")
+                    } header: {
+                     Text("Suarez")
                     }
-                    }
-                } label: {
-                    Text("Delete the tb")
                 }
-
+                .navigationTitle("My strategy")
+                }
+                .onAppear {
+                    UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)]
+                    UITableView.appearance().backgroundColor = #colorLiteral(red: 0.9586906126, green: 0.9586906126, blue: 0.9586906126, alpha: 1)
+                    
+                    BotAccountCoordinator.fetchBot()
+                            .receive(on: DispatchQueue.main)
+                            .sink { _ in
+            
+                            } receiveValue: { tb in
+                                viewModel.retrievals = tb
+                            }
+                            .store(in: &viewModel.subscribers)
+                }
             }
         }
-        .frame(width: viewModel.width, height: viewModel.height)
-    }
-}
+       
